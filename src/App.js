a50064e1139b05fac798446889fd4f23546b4d0d
jehielmartinez <jehielmartinez@gmail.com>
2020-02-06 13:24:23 -0600

    app layout styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import image from './cryptos.png';
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width:992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #FFF;
+  text-align: left;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+  }
+`;
 
 function App() {
   return (
-    <h1>Crypto</h1>
+    <Container>
+      <div>
+        <Image
+          src={image}
+          alt='crypto image'
+        />
+      </div>
+      <div>
+        <Heading>Quote your favorite Cryptocurrency</Heading>
+      </div>
+    </Container>
   );
 }
 
