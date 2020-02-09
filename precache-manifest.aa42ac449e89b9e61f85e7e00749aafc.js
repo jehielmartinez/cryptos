2ea273903e4a9d6adcc45039e53ff010e2a6cfc2
jehielmartinez <jehielmartinez@gmail.com>
2020-02-09 16:56:13 -0600

    Updates

---
+++ precache-manifest.aa42ac449e89b9e61f85e7e00749aafc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1a11ce789010834448744fdc427eac",
+    "url": "/cryptos/index.html"
+  },
+  {
+    "revision": "1e92a91c68e19dbd2740",
+    "url": "/cryptos/static/css/main.f5b9182b.chunk.css"
+  },
+  {
+    "revision": "11ce6a6b6c336ff78ea0",
+    "url": "/cryptos/static/js/2.610c7728.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cryptos/static/js/2.610c7728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e92a91c68e19dbd2740",
+    "url": "/cryptos/static/js/main.0315d058.chunk.js"
+  },
+  {
+    "revision": "8c984b5d87b364f062c7",
+    "url": "/cryptos/static/js/runtime-main.06d60839.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/cryptos/static/media/cryptos.96e45d69.png"
+  }
+]);
